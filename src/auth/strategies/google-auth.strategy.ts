ab42b0becf7d4be34de4c970d956dece9fee9c2c
yhd1101 <asd159369@naver.com>
2023-08-07 15:06:48 +0900

    done

--- src/auth/strategies/google-auth.strategy.ts
+++ src/auth/strategies/google-auth.strategy.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-google-oauth2';
 import { ConfigService } from '@nestjs/config';
 import { VerifyCallback } from 'passport-jwt';
+import { UserService } from '../../user/user.service';
+import { use } from 'passport';
+import { Provider } from '../../user/entities/provider.enum';
 
 @Injectable()
 export class GoogleAuthStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
     super({
       clientID: configService.get('GOOGLE_AUTH_CLIENTID'),
       clientSecret: configService.get('GOOGLE_AUTH_CLIENTSECRET'),
@@ -21,6 +27,34 @@ export class GoogleAuthStrategy extends PassportStrategy(Strategy, 'google') {
     profile: any,
     done: VerifyCallback,
   ): Promise<any> {
-    done(null, profile);
+    const { displayName, email, provider, picture } = profile;
+    const userInput = {
+      name: displayName,
+      email,
+      provider,
+      picture,
+    };
+    try {
+      const user = await this.userService.getUserByEmail(email);
+      //로그인 처리
+      if (user.provider !== provider) {
+        throw new HttpException(
+          `You are already subscribed to ${user.provider}`,
+          HttpStatus.CONFLICT,
+        );
+      }
+      done(null, user);
+    } catch (err) {
+      //이메일이 없으면 회원가입
+      if (err.status === 404) {
+        const newUser = await this.userService.createUser({
+          email,
+          name: displayName,
+          provider,
+          profileImg: picture,
+        });
+        done(null, newUser);
+      }
+    }
   }
 }
